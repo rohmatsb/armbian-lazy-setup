251d7fd68aa34051ffd74f9de0ba7c971c0d59d8
Rohmat Subekti <rohmatsb@gmail.com>
2025-02-14 11:57:43 +0700

    buat folder home untuk container

--- lazy-setup.sh
+++ lazy-setup.sh
@@ -7,6 +7,9 @@ sudo apt upgrade -y
 # Install BoxBuddy
 wget https://raw.githubusercontent.com/rohmatsb/boxbuddy-installer/main/install-boxbuddy.sh && chmod +x install-boxbuddy.sh && sudo ./install-boxbuddy.sh
 
+# Buat folder home untuk container
+mkdir Desktop/containers-homedir
+
 # Hapus residu file
 rm -rf ./install-boxbuddy.sh
 rm -rf ./lazy-setup.sh
