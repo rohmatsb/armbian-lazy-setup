86de3ad487b743bc1e00d9042c7896965904f46d
Rohmat Subekti <rohmatsb@gmail.com>
2025-02-14 10:22:21 +0700

    delete chromium installer

--- lazy-setup.sh
+++ lazy-setup.sh
@@ -4,9 +4,6 @@ sudo apt update
 # Upgrade semua paket
 sudo apt upgrade -y
 
-# Install Chromium (outside container)
-sudo apt install chromium -y
-
 # Install BoxBuddy
 wget https://raw.githubusercontent.com/rohmatsb/boxbuddy-installer/main/install-boxbuddy.sh && chmod +x install-boxbuddy.sh && sudo ./install-boxbuddy.sh
 
